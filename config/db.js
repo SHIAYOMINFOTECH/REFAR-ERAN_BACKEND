ab9c785a6eb59e2cf133f9eb9e6490bc06c6c94b
SHIVAYOM INFOTECH <shivayominfotech@gmail.com>
2025-04-23 15:51:43 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useCreateIndex: true, // optional: enables the creation of indexes
+      useFindAndModify: false, // optional: to avoid deprecation warnings for findAndModify
     });
     console.log('MongoDB connected');
   } catch (err) {
-    console.error('Error connecting to MongoDB', err);
+    console.error('MongoDB connection error:', err);
     process.exit(1);
   }
 };
